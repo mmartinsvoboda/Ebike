f0962b891705f05ca4c34c421dc1f06b450864fa
Martin Svoboda <martin.svoboda@etnetera.eu>
2023-04-17 17:43:48 +0200

    KDoc

--- app/src/test/java/com/svoboda/ebike/CalculateSpeedUseCaseTest.kt
+++ app/src/test/java/com/svoboda/ebike/CalculateSpeedUseCaseTest.kt
@@ -4,9 +4,16 @@ import com.svoboda.ebike.domain.usecase.CalculateSpeedUseCase
 import junit.framework.TestCase.assertEquals
 import org.junit.Test
 
+/**
+ * Test class for the [CalculateSpeedUseCase].
+ * It covers various test scenarios to ensure the correct calculation of speed.
+ */
 class CalculateSpeedUseCaseTest {
     private val calculateSpeedUseCase = CalculateSpeedUseCase()
 
+    /**
+     * Tests the speed calculation with sample values for rpm, gearRatio, and wheelSize.
+     */
     @Test
     fun `test speed calculation with sample values`() {
         val rpm = 3500
@@ -19,6 +26,9 @@ class CalculateSpeedUseCaseTest {
         assertEquals(expectedSpeed, actualSpeed, 0.01)
     }
 
+    /**
+     * Tests the speed calculation when the rpm value is zero.
+     */
     @Test
     fun `test speed calculation with zero rpm`() {
         val rpm = 0
@@ -31,6 +41,9 @@ class CalculateSpeedUseCaseTest {
         assertEquals(expectedSpeed, actualSpeed, 0.01)
     }
 
+    /**
+     * Tests the speed calculation with a different gear ratio value.
+     */
     @Test
     fun `test speed calculation with different gear ratio`() {
         val rpm = 3500
@@ -43,6 +56,9 @@ class CalculateSpeedUseCaseTest {
         assertEquals(expectedSpeed, actualSpeed, 0.01)
     }
 
+    /**
+     * Tests the speed calculation with another gear ratio value.
+     */
     @Test
     fun `test speed calculation with another gear ratio`() {
         val rpm = 3500
